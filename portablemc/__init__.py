2ac20c2950426015b7d57dc55815780b79737507
kokodev <github@kokofixcomputers.serv00.net>
2025-11-19 14:08:17 -0800

    Update __init__.py

--- portablemc/__init__.py
+++ portablemc/__init__.py
@@ -8,6 +8,6 @@ public API, and therefore stable across minor and patch version bumps.
 
 LAUNCHER_NAME = "portablemc"
 LAUNCHER_VERSION = "4.4.1"
-LAUNCHER_AUTHORS = ["Théo Rozier <contact@theorozier.fr>", "Contributors"]
+LAUNCHER_AUTHORS = ["Théo Rozier <contact@theorozier.fr>", "kokofixcomputers", "Contributors"]
 LAUNCHER_COPYRIGHT = "PortableMC  Copyright (C) 2021-2024  Théo Rozier"
-LAUNCHER_URL = "https://github.com/mindstorm38/portablemc"
+LAUNCHER_URL = "https://github.com/kokofixcomputers/portablemc"
